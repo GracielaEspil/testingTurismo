5785c02a9b1b2bd16ef31f73da70f69bc2db9ea6
Monica <mglfeliz@gmail.com>
2024-08-27 21:53:51 -0300

    constructores

---
+++ cypress/e2e/pages/pagconstruccionpage.page.js
@@ -0,0 +1,17 @@
+export class PagCostruccion{
+    logoWas(){
+        return cy.get("washatpp")
+    }
+    image(){
+        return cy.get("construction-image")
+    }
+    video(){
+        return cy.get("video-background")
+    }
+    textoT(){
+        return cy.get("h1-contains(text)")
+    }
+    textoE(){
+        return cy.get("p[contains(text()")
+    }
+}
