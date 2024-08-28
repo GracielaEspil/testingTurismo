6a25ed5261564dbc08ce5eef6301932069cbf641
Gustavo Menchaca <gusmenchaca@gmail.com>
2024-08-27 21:57:02 -0300

    selectores cypress

---
+++ cypress/e2e/pages/landingpage.page.js
@@ -0,0 +1,11 @@
+export class Landingpage {
+    logoWs(){
+        return cy.get(".whatsapp")
+    }
+
+    CarrouselTapalque(){
+        return cy.get(".Carrousel_active__Mg915")  
+    }
+
+    
+}
