a231ff90a372a6879366cabada6fdbe0044d8ac7
Graciela Espil <gwespil@gmail.com>
2024-10-29 16:40:16 -0300

    cambios de elemntos y selectores

--- cypress/e2e/pages/landingpage.page.js
+++ cypress/e2e/pages/landingpage.page.js
@@ -3,8 +3,44 @@ export class Landingpage {
         return cy.get(".whatsapp")
     }
 
-    CarrouselTapalque(){
-        return cy.get(".Carrousel_active__Mg915")  
+    imgDescubriTapalque(){
+        return cy.get("#banner")  
+    }
+
+    imgAzulOndas(){
+        return cy.get("img[alt='Ondas']")
+    }
+
+    imgTresEstrellas(){
+        return cy.get("body > main:nth-child(13) > section:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)")
+    }
+
+    textNegrita(){
+        return cy.get("body > main:nth-child(13) > section:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > p:nth-child(1)")
+    }
+
+    texto(){
+        return cy.get("body > main:nth-child(13) > section:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > p:nth-child(2)")
+    }
+
+    toponimo(){
+        return cy.get("#toponimo")
+    }
+
+    imgEvento(){
+        return cy.get("body > main:nth-child(13) > section:nth-child(4) > div:nth-child(1) > div:nth-child(1)")
+    }
+
+    imgGastronomia(){
+        return cy.get("body > main:nth-child(13) > section:nth-child(4) > div:nth-child(2) > div:nth-child(1)")
+    }
+
+    imgHospedaje(){
+        return cy.get("body > main:nth-child(13) > section:nth-child(4) > div:nth-child(3) > div:nth-child(1)")
+    }
+
+    comoLlegar(){
+        return cy.get(".bg-white.w-full.py-10.flex.justify-center")
     }
 
     
--- cypress/e2e/tests/test.test.js
+++ cypress/e2e/tests/test.test.js
@@ -1,6 +1,24 @@
 /// <reference types="cypress"/>
-describe('', () => {
-    it('Test 1', { tags: 't1' }, () => {
-        cy.log('Test #1')
+
+import { Landingpage } from "../pages/landingpage.page";
+
+describe('Landingpage', () => {
+    const landingpage = new Landingpage()
+    beforeEach(() =>{
+        cy.log(Cypress.env('baseUrl') + '/g')
+        cy.visit(Cypress.env('baseUrl') + '/g', { failOnStatusCode: false });
+    })
+    it('Pagina landing page' , { tags: 't1' }, () => {
+        cy.checkingnav()
+        cy.checkingfooter()
+        cy.chekingfiiterazuk()
+        landingpage.logoWs().should("be.visible")
+        landingpage.imgDescubriTapalque().should("be.visible")
+        landingpage.imgAzulOndas().should("be.visible")
+        landingpage.imgTresEstrellas().should("be.visible")
+        landingpage.textNegrita().should("be.visible")
+        landingpage.texto().should("be.visible")
+        landingpage.toponimo().should("be.visible")
+        
     });
 });
