6657dea013ef11dbcd30d680e46e0ab8eaacc929
Graciela Espil <gwespil@gmail.com>
2024-10-30 10:35:45 -0300

    cambio de pagina y selectores

--- cypress/e2e/tests/test.test.js
+++ cypress/e2e/tests/test.test.js
@@ -10,22 +10,10 @@ describe('Pag404page', () => {
         //("http://localhost:3000/g",{failOnStatusCode: false})
     })
     it('Pagina 404-vista', { tags: 't1' }, () => {
-        cy.checkingnav()
-        cy.checkingfooter()
-        cy.checkingfooterazul()
-        page404.textoerror().should("be.visible")
-        page404.textoH2().should("be.visible")
-        page404.imgLogo().should("be.visible")
-        page404.btnGastronimia().should("be.visible")
-        page404.btnHospedaje().should("be.visible")
-        page404.btnEventos().should("be.visible")
-        page404.btnQuienes().should("be.visible")
-        page404.btnHistoria().should("be.visible")
-        page404.btnTurRural().should("be.visible")
-        page404.btnAtractivos().should("be.visible")
-        page404.btnPanel().should("be.visible")
-        page404.btnRecupContra().should("be.visible")
-        page404.whatsapp().should("be.visible")
+        page404.imgDragon().should("be.visible")
+        page404.textoH1().should("be.visible")
+        page404.textoParrafo().should("be.visible")
+       
 
     });
 
